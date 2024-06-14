cfa6dcd96ec32c117852d87c8490c7a251e00c9f
pagar22 <aaryanpagar@hotmail.com>
2024-06-14 20:09:09 +0200

    cleanup: config files, remove unused dependencies

--- cloud-functions/scrape-candlesticks/utils/config.py
+++ cloud-functions/scrape-candlesticks/utils/config.py
@@ -1,14 +1,9 @@
 import logging as __logging
 import os
 
-from google.cloud import storage
-
 PROJECT_ID = os.getenv("PROJECT_ID")
-BUCKET_NAME = os.getenv("STORAGE_BUCKET")
 UPSTOX_API_URL = os.getenv("UPSTOX_API_URL")
 LAXMI_API_URL = os.getenv("LAXMI_API_URL")
 LAXMI_CUD_API_KEY = os.getenv("LAXMI_CUD_API_KEY")
 
-__storage_client = storage.Client()
 log = __logging.getLogger("cloud-functions.error")
-bucket = __storage_client.bucket(BUCKET_NAME)
