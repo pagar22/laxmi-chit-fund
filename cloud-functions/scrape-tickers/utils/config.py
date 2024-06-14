0a77a87fcab6c2302e56aa6c33077a6e72372cb7
pagar22 <aaryanpagar@hotmail.com>
2024-06-14 20:45:53 +0200

    fix: bucket name

--- cloud-functions/scrape-tickers/utils/config.py
+++ cloud-functions/scrape-tickers/utils/config.py
@@ -4,7 +4,7 @@ import os
 from google.cloud import storage
 
 PROJECT_ID = os.getenv("PROJECT_ID")
-BUCKET_NAME = os.getenv("STORAGE_BUCKET")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
 LAXMI_API_URL = os.getenv("LAXMI_API_URL")
 LAXMI_CUD_API_KEY = os.getenv("LAXMI_CUD_API_KEY")
 
